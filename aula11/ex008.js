f11583d6ada105f4967209b19adad4d7f95ae5cc
Adalmando <adalmando@adalmando.notebook>
2023-05-02 00:59:46 -0300

    nothing

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+console.log('Hello World!')
+
+var velocidade = 80
+
+// condicional ternário:
+velocidade > 80 ? console.log('Voce foi multado') : console.log('Voce não foi multado')
+// condição ? true : fase
+
+
+// condição simples, contém somente uma condição: 
+if(velocidade>80){
+    console.log('Você está dirigindo acima do limite de velocidade!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'
+var paisUp = pais.toUpperCase()
+
+console.log(`Vivendo em ${paisUp}`)
+if(paisUp == 'BRASIL'){
+    console.log('Você é brasileiro!')
+}else{
+    console.log('Você é estrangeiro!')
+}
